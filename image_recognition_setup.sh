1290ca7c8889c52104f27cc9cd1103e26516fa66
Bea <beanilsson1@gmail.com>
2017-09-08 07:30:59 +0000

    Add setup file for image recognition

---
+++ image_recognition_setup.sh
@@ -0,0 +1,9 @@
+echo Starting setup
+echo Upgrading pip
+python -m pip install --upgrade pip
+echo Installing numpy
+pip install numpy
+echo Installing matplotlib
+pip install matplotlib
+echo Installing pillow
+pip install pillow
