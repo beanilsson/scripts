e2dec4f24f4719fcdb5e9d0f34221f4b5b90a399
Bea <beanilsson1@gmail.com>
2017-09-08 16:26:04 +0200

    Update image_recognition_setup.sh

---
+++ image_recognition_setup.sh
@@ -0,0 +1,12 @@
+echo Starting setup
+sudo
+echo Upgrading pip
+python -m pip install --upgrade pip
+echo Installing numpy
+pip install numpy
+echo Installing matplotlib
+pip install matplotlib
+echo Installing pillow
+pip install pillow
+echo Installing OpenCV
+pip install opencv-python
